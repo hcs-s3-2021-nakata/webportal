762c8e27c7f94e25779a71b35787192a9e8a6a93
s20193088 <s20193088@HCS19093.stdlan.hcs.local>
2021-06-25 11:41:08 +0900

    最初のコミットです

---
+++ src/test/java/jp/ac/hcs/S3A323/WebportalApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.S3A323;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
